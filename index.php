5ceca9daa79642409ee239710f6bde15db3f1dcf
Litoprocess <maflores@litoprocess.com>
2022-07-22 10:43:35 -0500

    Agregando archivo perfiles.php al ultimo commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <section>
        <h2>Página de inicio</h2>
        <hr />
-       <p>Párrafo principal de la página de inicio</p>
+       <p>Párrafo principal de la página de inicio 1</p>
     </section>
     <footer>
     </footer>
