652691fd6b1f05d4237342c1e94192cc189f28dd
Litoprocess <maflores@litoprocess.com>
2022-07-22 10:40:36 -0500

    Agregando archivo index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <nav></nav>
+    <section>
+       <h2>Página de inicio</h2>
+       <hr />
+       <p>Párrafo principal de la página de inicio</p>
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
